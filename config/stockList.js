f7b674d640447515496aa25e45df0bb009db44b0
dnfvnsxn <dnfvnsxn@naver.com>
2020-05-01 22:37:17 +0900

    schedule and stock list change

--- config/stockList.js
+++ config/stockList.js
@@ -82,4 +82,100 @@ export const interests = [
     name: "브로드컴",
     symbol: "AVGO",
   },
+  {
+    name: "나이키",
+    symbol: "NKE",
+  },
+  {
+    name: "머크",
+    symbol: "MRK",
+  },
+  {
+    name: "알트리아",
+    symbol: "MO",
+  },
+  {
+    name: "월마트",
+    symbol: "WMT",
+  },
+  {
+    name: "홈디포",
+    symbol: "HD",
+  },
+  {
+    name: "버진갤럭틱홀딩스",
+    symbol: "SPCE",
+  },
+  {
+    name: "아마존닷컴",
+    symbol: "AMZN",
+  },
+  {
+    name: "넷플릭스",
+    symbol: "NFLX",
+  },
+  {
+    name: "비자",
+    symbol: "V",
+  },
+  {
+    name: "메인스트리트캐피탈 ",
+    symbol: "MAIN",
+  },
+  {
+    name: "엑슨모빌",
+    symbol: "XOM",
+  },
+  {
+    name: "애보트래버라토리",
+    symbol: "ABT",
+  },
+  {
+    name: "재너럴다이내믹스",
+    symbol: "GD",
+  },
+  {
+    name: "캐터필러",
+    symbol: "CAT",
+  },
+  {
+    name: "클로락스",
+    symbol: "CLX",
+  },
+  {
+    name: "하스브로",
+    symbol: "HAS",
+  },
+  {
+    name: "인텔",
+    symbol: "INTC",
+  },
+  {
+    name: "쉐브론",
+    symbol: "CVX",
+  },
+  {
+    name: "존슨앤존슨",
+    symbol: "JNJ",
+  },
+  {
+    name: "시스코",
+    symbol: "CSCO",
+  },
+  {
+    name: "버라이존",
+    symbol: "VZ",
+  },
+  {
+    name: "보잉",
+    symbol: "BA",
+  },
+  {
+    name: "3M",
+    symbol: "MMM",
+  },
+  {
+    name: "스타벅스",
+    symbol: "SBUX",
+  },
 ];
