1f57c4c8ac1f19565bac527a7139cd1d9fcf29d7
dnfvnsxn <dnfvnsxn@naver.com>
2020-05-05 21:29:08 +0900

    modify stock list

--- config/stockList.js
+++ config/stockList.js
@@ -178,4 +178,112 @@ export const interests = [
     name: "로스",
     symbol: "ROST",
   },
+  {
+    name: "액티비전 블리자드",
+    symbol: "ATVI",
+  },
+  {
+    name: "맥도날드",
+    symbol: "MCD",
+  },
+  {
+    name: "콘솔리데이티드에디슨",
+    symbol: "ED",
+  },
+  {
+    name: "채썸롯징트러스트",
+    symbol: "CLDT",
+  },
+  {
+    name: "아메리칸타워",
+    symbol: "AMT",
+  },
+  {
+    name: "아메리칸워터",
+    symbol: "AWK",
+  },
+  {
+    name: "메드트로닉",
+    symbol: "MDT",
+  },
+  {
+    name: "웰타워",
+    symbol: "WELL",
+  },
+  {
+    name: "암젠",
+    symbol: "AMGN",
+  },
+  {
+    name: "록히드마틴",
+    symbol: "LMT",
+  },
+  {
+    name: "메리어트",
+    symbol: "MAR",
+  },
+  {
+    name: "로우즈",
+    symbol: "LOW",
+  },
+  {
+    name: "카디널헬스",
+    symbol: "CAH",
+  },
+  {
+    name: "JP모건체이스",
+    symbol: "JPM",
+  },
+  {
+    name: "크로커",
+    symbol: "KR",
+  },
+  {
+    name: "프로로지스",
+    symbol: "PLD",
+  },
+  {
+    name: "넥스트이어러에너지",
+    symbol: "NEE",
+  },
+  {
+    name: "웨스트락",
+    symbol: "WRK",
+  },
+  {
+    name: "티로우프라이스그룹",
+    symbol: "TROW",
+  },
+  {
+    name: "애머런",
+    symbol: "AEE",
+  },
+  {
+    name: "포드",
+    symbol: "F",
+  },
+  {
+    name: "뉴코어",
+    symbol: "NUE",
+  },
+  {
+    name: "웰스파고",
+    symbol: "WFC",
+  },
+  {
+    name: "맥쿼리인프라",
+    symbol: "MIC",
+  },
+  {
+    name: "라스베이거스샌즈",
+    symbol: "LVS",
+  },
+  {
+    name: "LBrands",
+    symbol: "LB",
+  },
+  {
+    name: "엔비디아",
+    symbol: "NVDA",
+  },
 ];
